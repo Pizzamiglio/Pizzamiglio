de471da9af89c1457d52c0495b460b873a06ebf3
Pizzamiglio <102436755+Pizzamiglio@users.noreply.github.com>
2022-05-25 18:49:40 -0300

    Add files via upload

---
+++ Pagina-prueba/script.js
@@ -0,0 +1,33 @@
+/*
+console.log('Hola mundo')
+
+let firstname = 'Miguel'
+const lastName = 4
+
+console.log(firstname)
+
+
+
+SISTEMA BOOLEANO
+!= /*Operador de desigualdad*/
+/*Es decir que si en una funcion tenemos: 
+function testNotEqual(val) {
+  if (val !=) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+/*
+testNotEqual(10);
+
+La cadena va a devolver Not Equal cuando val no sea equivalente a 99, es decir que va a ser true en ese caso. 
+Caso contrario La cadena va a devolver Equal cuando val sea equivalente a 99, es decir va a ser false en ese caso.
+
+!== /*Operador de estricta desigualdad*/
+/*devuelve false cuando la comparación de estricta igualdad devolvería true y vice versa. 
+El operador de estricta desigualdad no convertirá los tipos de datos.
+==/*Operador de igualdad*/
+/*===/*Operador de estricta igualdad*
+/
+
+
